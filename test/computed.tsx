d3a0f0f682ffced2c3d56f4becc4064482ba8042
xgqfrms <xgqfrms@outlook.com>
2022-07-26 00:30:52 +0800

    计算属性

---
+++ test/computed.tsx
@@ -0,0 +1,19 @@
+import { render } from "solid-js/web";
+import { createSignal } from "solid-js";
+
+function Counter() {
+  const [count, setCount] = createSignal(0);
+
+  setInterval(() => setCount(count() + 1), 1000);
+  // const doubleCount = count() * 2;
+  // 计算属性
+  const doubleCount = () => count() * 2;
+  return <>
+    <div>Count: {count()}</div>
+    <div>doubleCount: {doubleCount()}</div>
+  </>;
+  // <div>doubleCount: {doubleCount()}</div>
+  // div>doubleCount: {doubleCount}</div>
+}
+
+render(() => <Counter />, document.getElementById('app'));
